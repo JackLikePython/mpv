7ac1e25b2f8920150093c1ff4dd2df85a2d4060e
告别记忆 <448384198@qq.com>
2022-09-19 13:28:56 +0800

    Add EasyBlend

--- video/out/opengl/context_win.c
+++ video/out/opengl/context_win.c
@@ -23,7 +23,8 @@
 #include "video/out/w32_common.h"
 #include "context.h"
 #include "utils.h"
-
+#include "scalable/EasyBlendSDK.h"
+#pragma comment(lib,"scalable/mplEasyBlendSDK.lib" )
 #if !defined(WGL_CONTEXT_MAJOR_VERSION_ARB)
 /* these are supposed to be defined in wingdi.h but mingw's is too old */
 /* only the bits actually used by mplayer are defined */
@@ -48,6 +49,8 @@ struct priv {
     HGLRC context;
     HDC hdc;
 };
+
+static EasyBlendSDK_Mesh *gMSDK;
 
 static void wgl_uninit(struct ra_ctx *ctx);
 
@@ -241,6 +244,9 @@ static bool compositor_active(struct ra_ctx *ctx)
 
 static void wgl_swap_buffers(struct ra_ctx *ctx)
 {
+    //Can Do It
+    EasyBlendSDKError msdkErr = EasyBlendSDK_TransformInputToOutput(gMSDK);
+    MP_VERBOSE(ctx->vo, "EasyBlendSDK Update = %d.\n", msdkErr);
     struct priv *p = ctx->priv;
     SwapBuffers(p->hdc);
 
@@ -300,6 +306,9 @@ static bool wgl_init(struct ra_ctx *ctx)
     if (!ra_gl_ctx_init(ctx, gl, params))
         goto fail;
 
+    gMSDK = new EasyBlendSDK_Mesh;
+    EasyBlendSDKError msdkErr = EasyBlendSDK_Initialize("D:\\SVN\\ControlCenterDaemon\\ControlCenterDaemon\\bin\\x64\\Release\\ScalableDataOrthographic.ol", gMSDK);
+    MP_VERBOSE(ctx->vo, "EasyBlendSDK Init = %d.\n", msdkErr);
     DwmEnableMMCSS(TRUE);
     return true;
 
@@ -343,6 +352,10 @@ static void wgl_uninit(struct ra_ctx *ctx)
 
     DwmEnableMMCSS(FALSE);
     vo_w32_uninit(ctx->vo);
+
+    EasyBlendSDKError msdkErr = EasyBlendSDK_Uninitialize(gMSDK);
+    delete gMSDK;
+    MP_VERBOSE(ctx->vo, "EasyBlendSDK UnInit = %d.\n", msdkErr);
 }
 
 static int wgl_control(struct ra_ctx *ctx, int *events, int request, void *arg)
